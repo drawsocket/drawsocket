5b229d94393483d41c61827050bf72580df27f4c
ramagottfried <rama.gottfried@gmail.com>
2021-05-04 13:25:17 +0200

    fixing path again, bump patch

--- drawsocket-server.js
+++ drawsocket-server.js
@@ -56,7 +56,7 @@ let post = console.log;
 let outlet = (msg) => {};
 
 let params = {
-    node_path: "./..",
+    node_path: "./node_modules/",
     userpath: null,
     htmltemplate: '/lib/drawsocket-page.html',
     infopage: "/lib/drawsocket-info.html",
